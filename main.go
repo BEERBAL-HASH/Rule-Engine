1ff812c1ef50dd6891a80723bf469bce5993e4c4
BEERBAL-HASH <patelbs98singh@gmail.com>
2023-05-05 01:31:41 +0530

    routes setup for triggers, actions and a new user

--- main.go
+++ main.go
@@ -4,9 +4,21 @@ import (
 	"fmt"
 
 	"github.com/BEERBAL-HASH/Rule-Engine/config"
-
+	"github.com/BEERBAL-HASH/Rule-Engine/controllers"
 	"github.com/gofiber/fiber/v2"
 )
+
+func SetupRoutes(app *fiber.App) {
+	app.Get("/getTriggers", controllers.GetTriggers)
+	app.Get("/getActions", controllers.GetActions)
+	app.Post("/user/new", controllers.NewUser)
+
+	//grouping for action handlers
+	action := app.Group("/Action")
+	action.Put("/arithmetic", controllers.ArithmeticAction)
+	action.Put("/update", controllers.UpdateAction)
+	action.Get("/plot", controllers.PlotAction)
+}
 
 func main() {
 	//welcome message
@@ -18,6 +30,8 @@ func main() {
 	//connect the database
 	config.ConnectDB()
 
+	SetupRoutes(app)
 	//server Listening on PORT 3000
 	app.Listen(":3000")
+
 }
